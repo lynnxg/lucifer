3043b01d9a3a3ea510d0f8752e333148fa64c3e7
Laura Brom <111150@st-maartenscollege.nl>
2020-06-25 20:55:04 +0000

    Introductie tekst

---
+++ main.py
@@ -0,0 +1,7 @@
+print('LUCIFERSPEL')
+print()
+print('Welkom bij het Luciferspel.')
+print('De computer en de speler (jij dus!) nemen om de beurt 1, 2 of 3 lucifers weg.')
+#er zijn in totaal tussen de 20/25 lucifers
+print('Degene die als laatste lucifers moet pakken, heeft verloren.')
+print('Success!')
