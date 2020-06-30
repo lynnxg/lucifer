7a5432f2180a2bfed3f643682fde75ea72ee0d7d
Laura Brom <111150@st-maartenscollege.nl>
2020-06-30 17:28:08 +0000

    Beurten (om de beurt iets kunnen doen)

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+#Begin bericht
 def welcome_message():
   print("LUCIFERSPEL")
   print()
   print('Welkom bij het Luciferspel.')
   print('De computer en de speler (jij dus!) nemen om de beurt 1, 2 of 3 lucifers weg.')
-  #er zijn in totaal tussen de 20/25 lucifers
   print('Degene die als laatste lucifers moet pakken, heeft verloren.')
   print('Success!')
 
@@ -11,6 +11,22 @@ welcome_message()
 
 import random
 aantallucifers = random.randint(20 , 25)
+print()
 print("We beginnen met %s lucifers." % aantallucifers)
 
+#Lopend spel
+
+def speler_actie():
+  turn = 1
+
+def computer_actie():
+  turn = 0
+
+#Beurten
+turn = 0
+
+if turn == 0:
+  speler_actie()
+else:
+  computer_actie()
 
